0cb40a76fdc35eceba0bc97c57912b689fd08d52
Pimkina Galina <novikova-box@yandex.ru>
2025-10-15 15:16:25 +0300

    add base class BaseModel

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from autoslug.fields import AutoSlugField
+
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
 # Create your models here.
