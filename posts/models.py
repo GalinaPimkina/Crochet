50e74ab001019f6be050a356e7530e253204d344
Pimkina Galina <novikova-box@yandex.ru>
2025-10-15 15:40:00 +0300

    BaseModelClass is abstract

--- posts/models.py
+++ posts/models.py
@@ -2,9 +2,13 @@ from django.db import models
 from autoslug.fields import AutoSlugField
 
 
-class BaseModel(models.Model):
+
+class BaseModelClass(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
 
 
 # Create your models here.
