d2a0763ee52b2608366ffddfdf402c193045375a
Pimkina Galina <novikova-box@yandex.ru>
2025-10-15 15:40:16 +0300

    create model Post

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from autoslug.fields import AutoSlugField
 
+from users.models import User
 
 
 class BaseModelClass(models.Model):
@@ -11,4 +12,10 @@ class BaseModelClass(models.Model):
         abstract = True
 
 
-# Create your models here.
+class Post(BaseModelClass):
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    image = models.ImageField(upload_to="image/%Y/%m/%d/", default=None, blank=True, null=True, verbose_name="Изображение")
+    body = models.TextField()
+    slug = AutoSlugField(populate_from="title", max_length=10, unique=True, db_index=True, verbose_name="Слаг")
+    author = models.ForeignKey(to=User, null=True, on_delete=models.SET_NULL, related_name="author")
+
